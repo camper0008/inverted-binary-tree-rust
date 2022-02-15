55789a3d004b21bea40f89911655d5da711b95c2
piet <42157469+camper0008@users.noreply.github.com>
2022-02-15 14:46:21 +0100

    fixed to be safe

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 mod node;
-mod stack;
 
-use crate::node::NodeType;
 use crate::node::Node;
 
 fn main() {
+    let mut tree = Node {
+        value: 9,
+        right: Box::new(Some(Node {
+            value: 2,
+            left: Box::new(Some(Node {
+                value: 6,
+                left: Box::new(None),
+                right: Box::new(None),
+            })),
+            right: Box::new(Some(Node {
+                value: 12,
+                left: Box::new(Some(Node {
+                    value: 2,
+                    left: Box::new(None),
+                    right: Box::new(None),
+                })),
+                right: Box::new(Some(Node {
+                    value: 4,
+                    left: Box::new(None),
+                    right: Box::new(None),
+                })),
+            })),
+        })),
+        left: Box::new(Some(Node {
+            value: 2,
+            left: Box::new(None),
+            right: Box::new(None),
+        })),
+    };
 
-    let l = Node::new(1, &mut NodeType::Null, &mut NodeType::Null);
-    let r = Node::new(2, &mut NodeType::Null, &mut NodeType::Null);
-    let node = &mut Node::new(5, &mut NodeType::Node(r), &mut NodeType::Node(l));
-    Node::print(*node, 0);
-    Node::swap_children(node);
-    Node::print(*node, 0);
+    tree.print(0);
+    tree = tree.recursive_revert();
+    tree.print(0);
 }
--- src/node.rs
+++ src/node.rs
@@ -1,61 +1,42 @@
-#[derive(Copy, Clone, PartialEq)]
-pub enum NodeType {
-    Null,
-    Node(Node),
-}
+use std::boxed::Box;
+use std::option::Option;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Node {
-    pub value:  i32,
-    pub left:   *mut NodeType,
-    pub right:  *mut NodeType,
+    pub value: i32,
+    pub left: Box<Option<Node>>,
+    pub right: Box<Option<Node>>,
 }
 
 impl Node {
-    pub fn new(value: i32, left: *mut NodeType, right: *mut NodeType) -> Self {
-        Self {
-            value,
-            left,
-            right,
-        }
-    }
-    pub fn swap_children(&mut self) {
-        let temp = self.left;
-        self.left = self.right;
-        self.right = temp;
-    }
-    pub fn print(self, level: usize) {
+    pub fn print(&self, level: usize) {
         println!("{}{}", " ".repeat(level), self.value);
-        unsafe {
-            match *self.left {
-                NodeType::Node(_) => {
-                    if let NodeType::Node(d) = *self.left {
-                        Node::print(d, level + 1);                        
-                    };
-                },
-                _ => {},
+        match &*self.left {
+            Some(n) => {
+                Node::print(&n, level + 1);
             }
+            _ => {}
         }
-        unsafe {
-            match *self.right {
-                NodeType::Node(_) => {
-                    if let NodeType::Node(d) = *self.right {
-                        Node::print(d, level + 1);                        
-                    };
-                },
-                _ => {},
+        match &*self.right {
+            Some(n) => {
+                Node::print(&n, level + 1);
             }
+            _ => {}
         }
     }
-    /* 
-    fn recursive_revert(&mut self) {
-
-    } 
-    fn stack_revert(&mut self) {
-
+    pub fn recursive_revert(self) -> Node {
+        let swapped_left = match *self.left {
+            Some(n) => Some(Self::recursive_revert(n)),
+            None => None,
+        };
+        let swapped_right = match *self.right {
+            Some(n) => Some(Self::recursive_revert(n)),
+            None => None,
+        };
+        Node {
+            value: self.value,
+            left: Box::new(swapped_right),
+            right: Box::new(swapped_left),
+        }
     }
-    fn free_node(&mut self) {
-
-    }
-    */
 }
